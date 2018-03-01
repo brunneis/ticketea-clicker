3813bea73ae5529d93fc9ac51aeae2bfbbba6e2c
Rodrigo Martínez <dev@brunneis.com>
2018-03-01 03:37:43 +0100

    Create clicker-cjs.js

---
+++ clicker-cjs.js
@@ -0,0 +1,42 @@
+/*  Ticketea Clicker for Custom JavaScript for websites
+ *  Copyright (C) 2018 Rodrigo Martínez <dev@brunneis.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const EVENT_URL = ""; // To fill
+var NO_TICKETS = 6;
+var NO_TRIES = 50;
+var SLEEP_MILLIS = 1;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+  if (window.location.href == EVENT_URL) {
+    $("#start-purchase-process-desktop").click();
+    var tries = 0;
+    const continueButton = $('#continue-button');
+    while (!continueButton.is(":visible")) {
+      $('#select-ticket-0').val(String(NO_TICKETS));
+      $('#select-ticket-0').change();
+      tries++;
+      if (tries > NO_TRIES) {
+        location.reload();
+        break;
+      }
+      await sleep(SLEEP_MILLIS);
+    }
+    continueButton.click();
+  }
+})();
